0c7a7f536c90cf023944f8d56502165f4aa668c2
Rebecca Olivieri <rebecca.olivieri2@studio.unibo.it>
2026-06-13 17:56:13 +0200

    Create Escursione

---
+++ src/main/java/it/unibo/destinationbuddy/data/Escursione.java
@@ -0,0 +1,28 @@
+package it.unibo.destinationbuddy.data;
+
+import java.time.LocalDate;
+
+public final class Escursione {
+
+    public final String idEscursione;
+    public final String titolo;
+    public final String difficolta;
+    public final double costo;
+    public final int postiDisponibili;
+    public final LocalDate dataAperturaEscursione;
+    public final LocalDate dataChiusuraEscursione;
+    //public final List<Certificazione> certificazioniRichieste;
+
+    public Escursione(String idEscursione, String titolo, String difficolta, double costo, int postiDisponibili,
+            LocalDate dataAperturaEscursione, LocalDate dataChiusuraEscursione) {
+        this.idEscursione = idEscursione == null ? "" : idEscursione;
+        this.titolo = titolo == null ? "" : titolo;
+        this.difficolta = difficolta == null ? "" : difficolta;
+        this.costo = costo;
+        this.postiDisponibili = postiDisponibili;
+        this.dataAperturaEscursione = dataAperturaEscursione;
+        this.dataChiusuraEscursione = dataChiusuraEscursione;
+    }
+
+
+}
