5bd8a4b948d5b64aeceeff8b7a2f4f14fa1f294c
Chiara Mazzoni <chiara.mazzoni12@studio.unibo.it>
2026-06-18 18:49:18 +0200

    dao

--- src/main/java/it/unibo/destinationbuddy/data/DAOUtils.java
+++ src/main/java/it/unibo/destinationbuddy/data/DAOUtils.java
@@ -1,4 +1,4 @@
-package it.unibo.destinationbuddy.data; // o data, in base a dove lo avete messo
+package it.unibo.destinationbuddy.data;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,18 +7,14 @@ import java.sql.SQLException;
 
 public final class DAOUtils {
 
-    // ==========================================
-    // AREA DA DECOMMENTARE IN BASE AL COMPUTER
-    // ==========================================
-    
+    // AREA DA DECOMMENTARE IN BASE AL S.O.
+
     // SE SEI SU PC WINDOWS LOCALE: Togli i commenti a questa riga
     private static final String URL = "jdbc:mysql://localhost:3306/DestinationBuddy";
     
     // SE SEI SU MAC (VS CODE SU MAC, DATABASE SU VM): Togli i commenti a questa riga
     //private static final String URL = "jdbc:mysql://10.211.55.3:3306/DestinationBuddy";
 
-    // ==========================================
-    
     private static final String USER = "app_java";
     private static final String PASSWORD = "DestinationBuddy_ACR";
 
@@ -27,18 +23,15 @@ public final class DAOUtils {
         try {
             return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (Exception e) {
-            // Se fallisce, usiamo la DAOException del prof!
             throw new DAOException(e);
         }
     }
 
-    // 2. IL METODO MAGICO DEL PROFESSORE PER LE QUERY
     public static PreparedStatement prepare(Connection connection, String query, Object... values) throws SQLException {
         PreparedStatement statement = null;
         try {
             statement = connection.prepareStatement(query);
             for (int i = 0; i < values.length; i++) {
-                // Questo ciclo imposta tutti i "?" in automatico!
                 statement.setObject(i + 1, values[i]);
             }
             return statement;
