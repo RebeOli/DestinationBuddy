4d1423d2aeaf3b7c4018d89a5392710d95d83404
Rebecca Olivieri <rebecca.olivieri2@studio.unibo.it>
2026-06-19 23:15:28 +0200

    fix conflicts

--- src/main/java/it/unibo/destinationbuddy/view/PremiumView.java
+++ src/main/java/it/unibo/destinationbuddy/view/PremiumView.java
@@ -50,6 +50,7 @@ public class PremiumView {
 
     private final Label errorLabel = new Label();
     private final ScrollPane root;
+    private final Label errorLabel;
 
     public PremiumView() {
         VBox page = new VBox(24);
